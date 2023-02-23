4991f193f018fb96147e0fc6e03a0553dda7e7d7
Aamir Shaikh <aamir0744@gmail.com>
2023-02-23 15:13:15 +0000

    done with models

---
+++ app/models/list.rb
@@ -0,0 +1,11 @@
+class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  def destroy
+    self.movies.destroy_all
+  end
+end
