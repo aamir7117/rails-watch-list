9ace5d99e2680c02aef44b1850971fa096255d96
Aamir Shaikh <aamir0744@gmail.com>
2023-02-24 15:07:23 +0000

    got app working

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @movie = Movie.find(params[:movie_id])
+    @bookmark.movie = @movie
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    # debugger
+  end
+
+  def show
+  end
+
+  def create
+    @bookmark = Bookmark.new(strong_params)
+    @bookmark.movie_id = params[:movie_id]
+    @bookmark.list_id = params[:list_id]
+    if @bookmark.save
+      redirect_to list_path(id: @bookmark.list_id)
+    else
+      debugger
+      render new(params), status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path
+  end
+
+  private
+
+  def strong_params
+    params.require(:bookmark).permit(:comment)
+  end
+end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
   def index
+    @movies = Movie.all
   end
 
   def show
+    @bookmark = Bookmark.new
   end
 end
