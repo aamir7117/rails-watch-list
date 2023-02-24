56ae6e804041a21947d062316f60f9784f7508a0
Aamir Shaikh <aamir0744@gmail.com>
2023-02-24 16:56:24 +0000

    added background image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # adding to enable cloudinary. Maps to storage.yml file Cloudinary object
+  config.active_storage.service = :cloudinary
 end
